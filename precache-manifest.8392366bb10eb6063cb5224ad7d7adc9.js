0f8ee84b9f3099c6d960fe484a290624b579a549
Rafler <amuronyk@gmail.com>
2019-09-25 20:45:16 +0300

    Updates

---
+++ precache-manifest.8392366bb10eb6063cb5224ad7d7adc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55ba9548b659286e75cbf36fea636bd7",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "ab8691aab2bcb0032ec7",
+    "url": "/react_tabs-with-router/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "dfe0cb6e46634b7ffc82",
+    "url": "/react_tabs-with-router/static/js/2.3ba721c2.chunk.js"
+  },
+  {
+    "revision": "ab8691aab2bcb0032ec7",
+    "url": "/react_tabs-with-router/static/js/main.07a27fb2.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
